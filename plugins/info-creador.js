5f694d6518b09cad8f8480d04169ed4a8d67a9fc
wa-bo <125730279+wa-bo@users.noreply.github.com>
2023-02-20 00:11:50 +1000

    ОЛРАПОЛ

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let text = `
-*—◉ НОМЕР МОЕГО СОЗДАТЕЛЯ wa.me/79524197466*
+*—◉ НОМЕР МОЕГО СОЗДАТЕЛЯ wa.me/79952800544*
 `.trim()   
 let buttonMessage= {
 'document': { url: `https://chat.whatsapp.com/CEYmCcV6UgO19tO3n56VOx` },
